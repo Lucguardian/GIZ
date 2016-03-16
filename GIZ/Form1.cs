e690f9fa045d7d405b3e12c42538ea2417ee238c
Lucas Trombini <lucastrombinilopes@gmail.com>
2016-03-15 21:11:49 -0300

    Inicial - tutorial do sharpmap até exemplo 2

---
+++ GIZ/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GIZ
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            SharpMap.Layers.VectorLayer vlay = new SharpMap.Layers.VectorLayer("States");
+            vlay.DataSource = new SharpMap.Data.Providers.ShapeFile("C:/Geoprocessamento/GIZ-shp/states_ugl.shp", true);
+            mapBox1.Map.Layers.Add(vlay);
+            mapBox1.Map.ZoomToExtents();
+            mapBox1.Refresh();
+            mapBox1.ActiveTool = SharpMap.Forms.MapBox.Tools.Pan;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
