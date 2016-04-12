a53adf66be8efe7c617b2e80c73999f905a5f9a1
Lucas Trombini <lucastrombinilopes@gmail.com>
2016-04-11 22:29:16 -0300

    Instalação do Gdal; Botões para adicionar layers raster-vetoriais.

--- GIZ/Form1.Designer.cs
+++ GIZ/Form1.Designer.cs
@@ -28,7 +28,15 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.mapBox1 = new SharpMap.Forms.MapBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.arquivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.tsbtnAddVector = new System.Windows.Forms.ToolStripButton();
+            this.tsbtnAddRaster = new System.Windows.Forms.ToolStripButton();
+            this.menuStrip1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // mapBox1
@@ -50,23 +58,84 @@
             this.mapBox1.Text = "mapBox1";
             this.mapBox1.WheelZoomMagnitude = -2D;
             // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.arquivoToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(889, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // arquivoToolStripMenuItem
+            // 
+            this.arquivoToolStripMenuItem.Name = "arquivoToolStripMenuItem";
+            this.arquivoToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.arquivoToolStripMenuItem.Text = "Arquivo";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsbtnAddVector,
+            this.tsbtnAddRaster});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 24);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(889, 25);
+            this.toolStrip1.TabIndex = 2;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // tsbtnAddVector
+            // 
+            this.tsbtnAddVector.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbtnAddVector.Image = ((System.Drawing.Image)(resources.GetObject("tsbtnAddVector.Image")));
+            this.tsbtnAddVector.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbtnAddVector.Name = "tsbtnAddVector";
+            this.tsbtnAddVector.Size = new System.Drawing.Size(23, 22);
+            this.tsbtnAddVector.Text = "toolStripButton1";
+            this.tsbtnAddVector.Click += new System.EventHandler(this.tsbtnAddVector_Click);
+            // 
+            // tsbtnAddRaster
+            // 
+            this.tsbtnAddRaster.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbtnAddRaster.Image = ((System.Drawing.Image)(resources.GetObject("tsbtnAddRaster.Image")));
+            this.tsbtnAddRaster.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbtnAddRaster.Name = "tsbtnAddRaster";
+            this.tsbtnAddRaster.Size = new System.Drawing.Size(23, 22);
+            this.tsbtnAddRaster.Text = "toolStripButton2";
+            this.tsbtnAddRaster.Click += new System.EventHandler(this.tsbtnAddRaster_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlDark;
             this.ClientSize = new System.Drawing.Size(889, 509);
+            this.Controls.Add(this.toolStrip1);
             this.Controls.Add(this.mapBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "GIZ";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private SharpMap.Forms.MapBox mapBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem arquivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton tsbtnAddVector;
+        private System.Windows.Forms.ToolStripButton tsbtnAddRaster;
     }
 }
 
--- GIZ/Form1.cs
+++ GIZ/Form1.cs
@@ -19,29 +19,29 @@ namespace GIZ
             InitializeComponent();
 
             //Cria o layer
-            SharpMap.Layers.VectorLayer vlay = new SharpMap.Layers.VectorLayer("States");
+            //SharpMap.Layers.VectorLayer vlay = new SharpMap.Layers.VectorLayer("States");
 
-            //Designa a fonte de dados
-            vlay.DataSource = new SharpMap.Data.Providers.ShapeFile("C:/Geoprocessamento/GIZ-shp/states_ugl.shp", true);
+            ////Designa a fonte de dados
+            //vlay.DataSource = new SharpMap.Data.Providers.ShapeFile("C:/Geoprocessamento/GIZ-shp/states_ugl.shp", true);
 
-            //Cria o estilo para Terra
-            SharpMap.Styles.VectorStyle landStyle = new SharpMap.Styles.VectorStyle();
-            landStyle.Fill = new System.Drawing.SolidBrush(System.Drawing.Color.FromArgb(232, 232, 232));
+            ////Cria o estilo para Terra
+            //SharpMap.Styles.VectorStyle landStyle = new SharpMap.Styles.VectorStyle();
+            //landStyle.Fill = new System.Drawing.SolidBrush(System.Drawing.Color.FromArgb(232, 232, 232));
 
-            //Cria o estilo para Água
-            SharpMap.Styles.VectorStyle waterStyle = new SharpMap.Styles.VectorStyle();
-            waterStyle.Fill = new System.Drawing.SolidBrush(System.Drawing.Color.FromArgb(198, 198, 255));
+            ////Cria o estilo para Água
+            //SharpMap.Styles.VectorStyle waterStyle = new SharpMap.Styles.VectorStyle();
+            //waterStyle.Fill = new System.Drawing.SolidBrush(System.Drawing.Color.FromArgb(198, 198, 255));
 
-            //Cria os itens do tema
-            Dictionary<string, SharpMap.Styles.IStyle> styles = new Dictionary<string, SharpMap.Styles.IStyle>();
-            styles.Add("land", landStyle);
-            styles.Add("water", waterStyle);
+            ////Cria os itens do tema
+            //Dictionary<string, SharpMap.Styles.IStyle> styles = new Dictionary<string, SharpMap.Styles.IStyle>();
+            //styles.Add("land", landStyle);
+            //styles.Add("water", waterStyle);
 
-            //Designa o tema
-            vlay.Theme = new SharpMap.Rendering.Thematics.UniqueValuesTheme<string>("class", styles, landStyle);
+            ////Designa o tema
+            //vlay.Theme = new SharpMap.Rendering.Thematics.UniqueValuesTheme<string>("class", styles, landStyle);
 
-            //Adiciona o layer ao mapa
-            mapBox1.Map.Layers.Add(vlay);
+            ////Adiciona o layer ao mapa
+            //mapBox1.Map.Layers.Add(vlay);
             
 
             ////ex 4 - layer wms
@@ -60,15 +60,15 @@ namespace GIZ
             //mapBox1.Map.Layers.Add(wmsL);
 
             //ex 5 - Tiled layer
-            ProjNet.CoordinateSystems.Transformations.CoordinateTransformationFactory ctfact = new ProjNet.CoordinateSystems.Transformations.CoordinateTransformationFactory();
-            vlay.CoordinateTransformation = ctfact.CreateFromCoordinateSystems(ProjNet.CoordinateSystems.GeographicCoordinateSystem.WGS84, ProjNet.CoordinateSystems.ProjectedCoordinateSystem.WebMercator);
-            vlay.ReverseCoordinateTransformation = ctfact.CreateFromCoordinateSystems(ProjNet.CoordinateSystems.ProjectedCoordinateSystem.WebMercator, ProjNet.CoordinateSystems.GeographicCoordinateSystem.WGS84);
+            //ProjNet.CoordinateSystems.Transformations.CoordinateTransformationFactory ctfact = new ProjNet.CoordinateSystems.Transformations.CoordinateTransformationFactory();
+            //vlay.CoordinateTransformation = ctfact.CreateFromCoordinateSystems(ProjNet.CoordinateSystems.GeographicCoordinateSystem.WGS84, ProjNet.CoordinateSystems.ProjectedCoordinateSystem.WebMercator);
+            //vlay.ReverseCoordinateTransformation = ctfact.CreateFromCoordinateSystems(ProjNet.CoordinateSystems.ProjectedCoordinateSystem.WebMercator, ProjNet.CoordinateSystems.GeographicCoordinateSystem.WGS84);
 
-            mapBox1.Map.BackgroundLayer.Add(new SharpMap.Layers.TileAsyncLayer(new BruTile.Web.OsmTileSource(), "OSM"));
+            //mapBox1.Map.BackgroundLayer.Add(new SharpMap.Layers.TileAsyncLayer(new BruTile.Web.OsmTileSource(), "OSM"));
 
-            mapBox1.Map.ZoomToExtents();
-            mapBox1.Refresh();
-            mapBox1.ActiveTool = SharpMap.Forms.MapBox.Tools.Pan;
+            //mapBox1.Map.ZoomToExtents();
+            //mapBox1.Refresh();
+            //mapBox1.ActiveTool = SharpMap.Forms.MapBox.Tools.Pan;
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -76,127 +76,168 @@ namespace GIZ
 
         }
 
-        protected override void OnLoad(EventArgs e)
+        private void tsbtnAddVector_Click(object sender, EventArgs e)
         {
-            var pt = new CustomMapTool.PopupTool();
-            MapBox _mapControl = null;
-            pt.MapControl = _mapControl;
-            pt.Enabled = true;
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.Filter = "arquivos shape (*.shp)|*.shp|Todos os arquivos (*.*)|*.*"; //define o tipo de arquivo q pode ser selecionado
+            openFileDialog1.FilterIndex = 1; //define qual das opções é padrão
+            openFileDialog1.RestoreDirectory = true;
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string x = openFileDialog1.FileName; //path inteiro do shape
+                string y = openFileDialog1.SafeFileName; //apenas nome e extensão do arquivo
+                SharpMap.Layers.VectorLayer vlay = new SharpMap.Layers.VectorLayer(y);
+                vlay.DataSource = new SharpMap.Data.Providers.ShapeFile(x, true);
+                mapBox1.Map.Layers.Add(vlay);
+                mapBox1.Map.ZoomToExtents();
+                mapBox1.Refresh();
+                mapBox1.ActiveTool = MapBox.Tools.Pan;
+            }
         }
+
+        private void tsbtnAddRaster_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.Filter = "arquivos jpg (*.jpg)|*.jpg|arquivos tiff (*.tif)|*.tif|todos os arquivos (*.*)|*.*"; //define o tipo de arquivo q pode ser selecionado
+            openFileDialog1.FilterIndex = 2; //define qual das opções é padrão
+            openFileDialog1.RestoreDirectory = true;
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string x = openFileDialog1.FileName; //path inteiro do raster
+                string y = openFileDialog1.SafeFileName; //apenas nome e extensão do arquivo
+                SharpMap.Layers.GdiImageLayer rlay = new SharpMap.Layers.GdiImageLayer(x);
+                mapBox1.Map.Layers.Add(rlay);
+                mapBox1.Map.ZoomToExtents();
+                mapBox1.Refresh();
+                mapBox1.ActiveTool = MapBox.Tools.Pan;
+            }
+        }
+
+        //protected override void OnLoad(EventArgs e)
+        //{
+        //    var pt = new CustomMapTool.PopupTool();
+        //    MapBox _mapControl = null;
+        //    pt.MapControl = _mapControl;
+        //    pt.Enabled = true;
+        //}
     }
 
-    public abstract class CustomMapTool
-    {
-        private MapBox _mapControl;
-        private bool _enabled;
+    //public abstract class CustomMapTool
+    //{
+    //    private MapBox _mapControl;
+    //    private bool _enabled;
 
-        /// <summary>
-        /// Event raised when the <see cref="MapControl"/> is about to change.
-        /// </summary>
-        public event CancelEventHandler MapControlChanging;
+    //    /// <summary>
+    //    /// Event raised when the <see cref="MapControl"/> is about to change.
+    //    /// </summary>
+    //    public event CancelEventHandler MapControlChanging;
 
-        /// <summary>
-        /// Event invoker for the <see cref="MapControlChanging"/> event
-        /// </summary>
-        /// <remarks>Override this method to unwire <see cref="MapBox"/>s events. Don't forget to call <c>base.OnMapControlChanging(cea);</c> to invoke the event.</remarks>
-        /// <param name="cea">The cancel event arguments</param>
-        protected virtual void OnMapControlChanging(CancelEventArgs cea)
-        {
-            var h = MapControlChanging;
-            if (h != null) h(this, cea);
-        }
+    //    /// <summary>
+    //    /// Event invoker for the <see cref="MapControlChanging"/> event
+    //    /// </summary>
+    //    /// <remarks>Override this method to unwire <see cref="MapBox"/>s events. Don't forget to call <c>base.OnMapControlChanging(cea);</c> to invoke the event.</remarks>
+    //    /// <param name="cea">The cancel event arguments</param>
+    //    protected virtual void OnMapControlChanging(CancelEventArgs cea)
+    //    {
+    //        var h = MapControlChanging;
+    //        if (h != null) h(this, cea);
+    //    }
 
-        /// <summary>
-        /// Event raised when the <see cref="MapControl"/> has changed.
-        /// </summary>
-        public event EventHandler MapControlChanged;
+    //    /// <summary>
+    //    /// Event raised when the <see cref="MapControl"/> has changed.
+    //    /// </summary>
+    //    public event EventHandler MapControlChanged;
 
-        /// <summary>
-        /// The event invoker 
-        /// </summary>
-        /// <remarks>Override this method to wire to <see cref="MapBox"/>s events. Don't forget to call <c>base.OnMapControlChanged(e);</c> to invoke the event.</remarks>
-        /// <param name="e">The event arguments</param>
-        protected virtual void OnMapControlChanged(EventArgs e)
-        {
-            var h = MapControlChanged;
-            if (h != null) h(this, e);
-        }
+    //    /// <summary>
+    //    /// The event invoker 
+    //    /// </summary>
+    //    /// <remarks>Override this method to wire to <see cref="MapBox"/>s events. Don't forget to call <c>base.OnMapControlChanged(e);</c> to invoke the event.</remarks>
+    //    /// <param name="e">The event arguments</param>
+    //    protected virtual void OnMapControlChanged(EventArgs e)
+    //    {
+    //        var h = MapControlChanged;
+    //        if (h != null) h(this, e);
+    //    }
 
-        /// <summary>
-        /// Gets or sets a value indicating the map control
-        /// </summary>
-        public SharpMap.Forms.MapBox MapControl
-        {
-            get { return _mapControl; }
-            set
-            {
-                if (value == _mapControl)
-                    return;
+    //    /// <summary>
+    //    /// Gets or sets a value indicating the map control
+    //    /// </summary>
+    //    public SharpMap.Forms.MapBox MapControl
+    //    {
+    //        get { return _mapControl; }
+    //        set
+    //        {
+    //            if (value == _mapControl)
+    //                return;
 
-                var cea = new CancelEventArgs(false);
-                OnMapControlChanging(cea);
-                if (cea.Cancel) return;
-                _mapControl = value;
-                OnMapControlChanged(EventArgs.Empty);
-            }
-        }
+    //            var cea = new CancelEventArgs(false);
+    //            OnMapControlChanging(cea);
+    //            if (cea.Cancel) return;
+    //            _mapControl = value;
+    //            OnMapControlChanged(EventArgs.Empty);
+    //        }
+    //    }
 
-        /// <summary>
-        /// Event raised when <see cref="Enabled"/> changed.
-        /// </summary>
-        public event EventHandler EnabledChanged;
+    //    /// <summary>
+    //    /// Event raised when <see cref="Enabled"/> changed.
+    //    /// </summary>
+    //    public event EventHandler EnabledChanged;
 
-        /// <summary>
-        /// Event invoker for the <see cref="EnabledChanged"/> event.
-        /// </summary>
-        /// <param name="e"></param>
-        protected virtual void OnEnabledChanged(EventArgs e)
-        {
-            var h = EnabledChanged;
-            if (h != null) h(this, e);
-        }
+    //    /// <summary>
+    //    /// Event invoker for the <see cref="EnabledChanged"/> event.
+    //    /// </summary>
+    //    /// <param name="e"></param>
+    //    protected virtual void OnEnabledChanged(EventArgs e)
+    //    {
+    //        var h = EnabledChanged;
+    //        if (h != null) h(this, e);
+    //    }
 
-        /// <summary>
-        /// Gets or sets a value indicating if the tool is enabled or not
-        /// </summary>
-        public bool Enabled
-        {
-            get { return _mapControl != null && _enabled; }
-            set
-            {
-                if (value == _enabled)
-                    return;
-                _enabled = value;
-                OnEnabledChanged(EventArgs.Empty);
-            }
-        }
+    //    /// <summary>
+    //    /// Gets or sets a value indicating if the tool is enabled or not
+    //    /// </summary>
+    //    public bool Enabled
+    //    {
+    //        get { return _mapControl != null && _enabled; }
+    //        set
+    //        {
+    //            if (value == _enabled)
+    //                return;
+    //            _enabled = value;
+    //            OnEnabledChanged(EventArgs.Empty);
+    //        }
+    //    }
 
-        public class PopupTool : CustomMapTool
-        {
-            protected override void OnMapControlChanging(CancelEventArgs cea)
-            {
-                base.OnMapControlChanging(cea);
-                if (cea.Cancel) return;
+    //    public class PopupTool : CustomMapTool
+    //    {
+    //        protected override void OnMapControlChanging(CancelEventArgs cea)
+    //        {
+    //            base.OnMapControlChanging(cea);
+    //            if (cea.Cancel) return;
 
-                if (MapControl == null) return;
-                MapControl.MouseDown -= HandleMouseDown;
-            }
+    //            if (MapControl == null) return;
+    //            MapControl.MouseDown -= HandleMouseDown;
+    //        }
 
-            protected override void OnMapControlChanged(EventArgs e)
-            {
-                base.OnMapControlChanged(e);
-                if (MapControl == null) return;
-                MapControl.MouseDown += HandleMouseDown;
-            }
+    //        protected override void OnMapControlChanged(EventArgs e)
+    //        {
+    //            base.OnMapControlChanged(e);
+    //            if (MapControl == null) return;
+    //            MapControl.MouseDown += HandleMouseDown;
+    //        }
 
-            private void HandleMouseDown(Coordinate worldpos, MouseEventArgs imagepos)
-            {
-                if (!Enabled)
-                    return;
+    //        private void HandleMouseDown(Coordinate worldpos, MouseEventArgs imagepos)
+    //        {
+    //            if (!Enabled)
+    //                return;
 
-                MessageBox.Show(string.Format("You clicked at {0}!", worldpos));
-                Enabled = false;
-            }
-        }
-    }
+    //            MessageBox.Show(string.Format("You clicked at {0}!", worldpos));
+    //            Enabled = false;
+    //        }
+    //    }
+    //}
 }
